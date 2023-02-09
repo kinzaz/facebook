c6aa380f94e0d309ba33cd3fb0c99e2d8601ec55
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-02-09 23:13:22 +0300

    Настройка cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const app = express();
+
+var corsOptions = {
+	origin: 'http://localhost:3000',
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
 	res.send('Welcome from home');

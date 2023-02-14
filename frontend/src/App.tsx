26722135c2448ce03012ae5ad7e910cf4209ac79
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-02-14 19:55:58 +0300

    Начальная настройка react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/home';
+import { Login } from './pages/login';
+import { Profile } from './pages/profile';
 
 const App = (): JSX.Element => {
-	return <div></div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/profile" element={<Profile />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default App;

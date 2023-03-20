657a93f349dc237d24654c9e183d0c76e0e1b582
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-03-20 20:04:29 +0300

    sass-resources-loader for utils.scss

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,153 +1,3 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-// const TerserPlugin = require('terser-webpack-plugin');
-
-// const isDev = process.env.NODE_ENV === 'development';
-// const isProd = process.env.NODE_ENV !== 'development';
-// const filename = ext => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
-
-// const cssLoaders = extra => {
-// 	const loaders = [
-// 		{
-// 			loader: MiniCssExtractPlugin.loader,
-// 			options: {},
-// 		},
-// 		'css-loader',
-// 	];
-// 	if (extra) {
-// 		loaders.push(extra);
-// 	}
-
-// 	return loaders;
-// };
-
-// const plugins = [
-// 	new HtmlWebpackPlugin({
-// 		template: './index.html',
-// 		minify: {
-// 			collapseWhitespace: isProd && true,
-// 		},
-// 	}),
-// 	new CleanWebpackPlugin(),
-// 	new CopyPlugin({
-// 		patterns: [
-// 			{
-// 				from: path.resolve(__dirname, 'src/assets'),
-// 				to: path.resolve(__dirname, 'public/assets'),
-// 			},
-// 		],
-// 	}),
-// 	new MiniCssExtractPlugin({ filename: filename('css') }),
-// ];
-
-// module.exports = {
-// 	context: path.resolve(__dirname, 'src'),
-// 	mode: 'development',
-// entry: {
-// 	main: './index.tsx',
-// },
-// 	output: {
-// 		filename: filename('js'),
-// 		path: path.resolve(__dirname, 'public'),
-// 	},
-// 	resolve: {
-// 		extensions: ['.js', '.json', '.css', 'scss', 'jsx', '.ts', '.tsx'],
-// 		alias: {
-// 			'@': path.resolve(__dirname, 'src'),
-// 			'@modules': path.resolve(__dirname, 'src/modules'),
-// 			'@pages': path.resolve(__dirname, 'src/pages'),
-// 			'@styles': path.resolve(__dirname, 'src/styles'),
-// 			'@shared': path.resolve(__dirname, 'src/shared'),
-// 			'@hooks': path.resolve(__dirname, 'src/hooks'),
-// 		},
-// 	},
-// 	optimization: {
-// 		splitChunks: {
-// 			chunks: 'all',
-// 		},
-// 		minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-// 	},
-// 	plugins,
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.(jsx|js)$/,
-// 				include: path.resolve(__dirname, 'src'),
-// 				exclude: /node_modules/,
-// 				use: [
-// 					{
-// 						loader: 'babel-loader',
-// 						options: {
-// 							presets: [
-// 								[
-// 									'@babel/preset-env',
-// 									{
-// 										targets: 'defaults',
-// 									},
-// 								],
-// 								'@babel/preset-react',
-// 							],
-// 						},
-// 					},
-// 				],
-// 			},
-// 			{
-// 				test: /\.(tsx|ts)$/,
-// 				include: path.resolve(__dirname, 'src'),
-// 				exclude: /node_modules/,
-// 				use: [
-// 					{
-// 						loader: 'babel-loader',
-// 						options: {
-// 							presets: [
-// 								[
-// 									'@babel/preset-env',
-// 									{
-// 										targets: 'defaults',
-// 									},
-// 								],
-// 								'@babel/preset-react',
-// 							],
-// 						},
-// 					},
-// 				],
-// 			},
-// 			{
-// 				test: /\.css$/i,
-// 				use: cssLoaders(),
-// 			},
-// 			{
-// 				test: /\.s[ac]ss$/i,
-// 				use: cssLoaders('sass-loader'),
-// 			},
-// 			{
-// 				test: /\.(png|jpg|svg|gif)$/,
-// 				type: 'asset/resource',
-// 			},
-// 			{
-// 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-// 				type: 'asset/resource',
-// 			},
-// 			{
-// 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-// 				type: 'asset/resource',
-// 			},
-// 			{
-// 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-// 				type: 'asset/resource',
-// 			},
-// 			{
-// 				test: /\.(csv|tsv)$/i,
-// 				use: ['csv-loader'],
-// 			},
-// 		],
-// 	},
-// };
-
 const path = require('path');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -264,6 +114,17 @@ module.exports = {
 			{
 				test: /\.s[ac]ss$/i,
 				use: cssLoaders('sass-loader'),
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'sass-resources-loader',
+						options: {
+							resources: path.resolve(__dirname, 'src/styles/utils.scss'),
+						},
+					},
+				],
 			},
 			{
 				test: /\.(png|jpg|svg|gif)$/,
